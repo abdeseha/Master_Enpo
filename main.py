43fa0745743133a85155f2b34eb436aa67890101
ashjmsegs@gmail.com <ashjmsegs@gmail.com>
2023-07-25 21:38:31 +0100

    starting_main_2

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
 
-from Caculate import *
+from caculate import *
 from ploting import *
-if input('Read new file (Y/N): ').upper == 'Y' or 'YES':
-    data = pd.read_excel(input('Data file: '))
-    data.to_csv(r"data.csv", index= None, header=True)
 
-data = pd.read_csv("data.csv", nrows=10000, skiprows=0)
+counter = 0
 
-print(data)
+rd = input('Read new file (Y/N): ').upper()
+if  rd == 'Y' or rd == 'YES':
+    data = pd.read_excel(input('Data file: '), index = True)
+    data.to_csv(r"data.csv")
+
+while True:
+    data = pd.read_csv("data.csv", nrows=10000, skiprows=10000*counter)
+    print(data[0])
+    #P = Elt_power(data[])
