131c2efe14a3ecc5aa670892b9a6f61f55981dc5
ashjmsegs@gmail.com <ashjmsegs@gmail.com>
2023-07-25 21:37:52 +0100

    starting_main

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import pandas as pd
 import numpy as np
+
+from Caculate import *
+from ploting import *
+if input('Read new file (Y/N): ').upper == 'Y' or 'YES':
+    data = pd.read_excel(input('Data file: '))
+    data.to_csv(r"data.csv", index= None, header=True)
+
+data = pd.read_csv("data.csv", nrows=10000, skiprows=0)
+
+print(data)
