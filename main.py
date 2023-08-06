9c97754fd4ac7e1a25df483dbdff1010864e76e6
ashjmsegs@gmail.com <ashjmsegs@gmail.com>
2023-08-06 22:14:11 +0100

    final touches

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ skiprow = 1000
 marge = 20
 Operations = {}
 
-# rd = input('Read new file (Y/N): ').upper()
-# if  rd == 'Y' or rd == 'YES':
-#     data = pd.read_excel(input('Data file: '))
-#     data.to_csv(r"./data/data.csv")
+rd = input('Read new file (Y/N): ').upper()
+if  rd == 'Y' or rd == 'YES':
+    data_xl = pd.read_excel(input('Data file: '))
+    data_xl.to_csv(r"./data/data.csv", index=False)
 
 while True:
     data = pd.read_csv("./data/data.csv", nrows=row, skiprows=skiprow*counter)
@@ -24,14 +24,9 @@ while True:
     P = Calculate.pow(data.iloc[:,1],data.iloc[:,2],data.iloc[:,3])
     P_mavg = Calculate.mob_avg(P,num_vals=20,times=5)
     P_mavg_div = Calculate.div(data.iloc[:,0], P_mavg)
-    graph = plot(data.iloc[:,0], Preassure = P, P_avg=P_mavg)
+    graph = plot(data.iloc[:,0], Preassure = P)
 
     Characterize.characterize(time=data.iloc[:,0], org_values=P_mavg, dives=P_mavg_div)
-    for key, value in Characterize.operations.items():
-        print(F'{key} :')
-        for key_1, value_1 in value.items():
-            print(F'----> {key_1} : {value_1}')
-    print('-----------------------------------')
     graph.plt_graph()
     graph.shade(Characterize.operations)
     
