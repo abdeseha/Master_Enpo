291485fd8dad725231458d48e9e9760f1ee62949
ashjmsegs@gmail.com <ashjmsegs@gmail.com>
2023-08-06 05:12:14 +0100

    charactirization step mostly done :)

--- calculate.py
+++ calculate.py
@@ -1,26 +1,86 @@
 import numpy as np
 
-class calculate:
-    def Pow(I_1, I_2, I_3, V=None):
+class Calculate:
+    def pow(I_1, I_2, I_3, V=None):
         if V is None:
             V = 400
         power = (I_1 + I_2 + I_3) * V
         return power
     
-    def mob_avg(list_in=None,range=1):
-
+    def mob_avg(list_in=None, num_vals=1, times=1):
         avrgs = np.empty((0))
-        i = 0
-
-        while len(list_in) >= range + i:
-            avrgs = np.append(avrgs, [np.average(list_in[i:range + i-1])], axis=0)
-            i += 1
+        for _ in range(times):
+            if len(avrgs) == 0:
+                i = 0
+                while len(list_in) >= num_vals + i:
+                    avrgs = np.append(avrgs, [np.average(list_in[i:num_vals + i])], axis=0)
+                    i += 1
+            else:
+                i = 0
+                list_in = avrgs
+                avrgs = np.empty((0))
+                while len(list_in) >= num_vals + i:
+                    avrgs = np.append(avrgs, [np.average(list_in[i:num_vals + i])], axis=0)
+                    i += 1
         return avrgs
 
     def div(time, vals):
         shift = abs(len(time) - len(vals))
         divs = np.empty((0))
-        for i, value in enumerate(vals[1:]):
-             divs = np.append(divs, [(value - vals[i-1])/(time[i+shift] - time[i+shift -1])], axis=0)
+        for i in range(1,len(vals)):
+            divs = np.append(divs, [(vals[i] - vals[i-1])/(time[i+shift] - time[i+shift -1])], axis=0)
         return divs
     
+class Characterize:
+    last_time = 0
+    operations = {}
+    operation = {'nb_of_use':1, 'stab_value':0, 'start_time':np.array([0]), 'end_time':np.array([0]), 'max':np.array([0])}
+    operation_num = 1
+    in_opt = False
+    
+    def add_operation():
+        if len(Characterize.operations) == 0:
+            Characterize.operations[F'operation_{Characterize.operation_num}'] = Characterize.operation
+        else:
+            for opt in Characterize.operations.values():
+                if abs(Characterize.operation['stab_value'] - opt['stab_value']) < Characterize.operation['stab_value']*0.03:
+                    opt['nb_of_use'] += 1
+                    opt['start_time'] = np.append(opt['start_time'], Characterize.operation['start_time'])
+                    opt['end_time'] = np.append(opt['end_time'], Characterize.operation['end_time'])
+                    opt['max'] = np.append(opt['max'], Characterize.operation['max'])
+                
+                else:
+                    Characterize.operations[F'operation_{Characterize.operation_num}'] = Characterize.operation
+                    Characterize.operation_num += 1
+
+    def characterize(time=None, org_values=None, meaned_values=None, dives=None):
+        
+        if meaned_values is None :
+            meaned_values = org_values
+
+        stab_value = np.empty((0))
+        shift_time = len(time)-len(dives)
+        shift_org = len(org_values)-len(dives)
+        shift_meaned = len(meaned_values)-len(dives)
+
+        for i,dive in enumerate(dives):
+            if  time[shift_time+i] >= Characterize.last_time:
+                if org_values[shift_org+i] > Characterize.operation['max'][0]:
+                    Characterize.operation['max'][0] = org_values[shift_org+i]
+
+                if dive > 5 and Characterize.in_opt == False:
+                    Characterize.operation['start_time'][0] = time[shift_time+i]
+                    Characterize.in_opt = True
+                    
+                elif abs(dive) < 5 and meaned_values[shift_meaned+i] > 1000  and  Characterize.in_opt == True:
+                    stab_value = np.append(stab_value, [meaned_values[shift_meaned+i]])
+                    stab_value = np.array([np.average(stab_value)])
+                    Characterize.operation['stab_value'] = stab_value[0]
+
+                elif abs(dive) < 5 and meaned_values[shift_meaned+i] < 100 and Characterize.in_opt == True:
+                    Characterize.operation['end_time'] = time[shift_time+i]
+                    Characterize.add_operation()
+                    Characterize.operation =  {'nb_of_use':1, 'stab_value':0, 'start_time':np.array([0]), 'end_time':np.array([0]), 'max':np.array([0])}
+                    Characterize.in_opt = False                    
+                     
+        Characterize.last_time = time.iloc[-1]
--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from ploting import *
 counter = 0
 row = 10000
 skiprow = 1000
-marge = 40
+marge = 20
+Operations = {}
 
-rd = input('Read new file (Y/N): ').upper()
-if  rd == 'Y' or rd == 'YES':
-    data = pd.read_excel(input('Data file: '))
-    data.to_csv(r"./data/data.csv")
+# rd = input('Read new file (Y/N): ').upper()
+# if  rd == 'Y' or rd == 'YES':
+#     data = pd.read_excel(input('Data file: '))
+#     data.to_csv(r"./data/data.csv")
 
 while True:
     data = pd.read_csv("./data/data.csv", nrows=row, skiprows=skiprow*counter)
@@ -20,12 +21,20 @@ while True:
         counter -= 1
         data = pd.read_csv("./data/data.csv", nrows=row, skiprows=skiprow*counter)
     
-    P = calculate.Pow(data.iloc[:,1],data.iloc[:,2],data.iloc[:,3])
-    P_mavg_1 = calculate.mob_avg(P,range=marge)
-    P_mavg_2 = calculate.mob_avg(P_mavg_1, range=marge)
-    P_mavg_2_div = calculate.div(data.iloc[:,0], P_mavg_2)
-    graph = plot(data.iloc[:,0], prussur = P)
-    graph.set_anime()
+    P = Calculate.pow(data.iloc[:,1],data.iloc[:,2],data.iloc[:,3])
+    P_mavg = Calculate.mob_avg(P,num_vals=20,times=5)
+    P_mavg_div = Calculate.div(data.iloc[:,0], P_mavg)
+    graph = plot(data.iloc[:,0], Preassure = P, P_avg=P_mavg)
+
+    Characterize.characterize(time=data.iloc[:,0], org_values=P_mavg, dives=P_mavg_div)
+    for key, value in Characterize.operations.items():
+        print(F'{key} :')
+        for key_1, value_1 in value.items():
+            print(F'----> {key_1} : {value_1}')
+    
+    graph.plt_graph()
+    graph.shade(Characterize.operations)
+    
     counter += 1
 
 plt.show()
